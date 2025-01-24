717a26dd3891930a73dff89d048592d86cc665db
sathyamurali <sathya.murali@gmail.com>
2025-01-24 12:24:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const BASEURL="https://script.google.com/macros/s/AKfycbyCQwuOOMkRPWCOzV2m2cZvncQxIPN2n9tjh-LE78IUVFIQI2dz0HJdllLIxOm3alOX/exec";
+
 function changeIframeSource() {
-  const iframe = document.getElementById('myIframe');
-  const source = BASEURL+"?mode=admin";
-  iframe.src = source;
+	const fullURL = document.location.href;
+	const filename = fullURL.substring(fullURL.lastIndexOf('/') + 1);
+	let iframeUrlParam
+	switch(filename){
+	case "index.html":
+		iframeUrlParam="?mode=simple";
+		break;
+	case "manager.html"
+		iframeUrlParam="?mode=admin";
+		break;
+	default:
+		iframeUrlParam=""
+		break;
+	}
+	const iframe = document.getElementById('myIframe');
+	iframe.src = BASEURL+iframeUrlParam;
 }
