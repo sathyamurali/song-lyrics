b9e8845914bce4dc0cb2172c032c965c852dbaf0
sathyamurali <sathya.murali@gmail.com>
2025-04-03 09:40:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASEURL="https://script.google.com/macros/s/AKfycbyuXo7Ml6dsPgE_VRCec4anHaWQTWmSWZUD1xTmG1A2Rn0kM1Z09jJOEyDe2Syi7l2lMw/exec";
+const BASEURL="https://script.google.com/macros/s/AKfycbw7Iun6vjAnecjKSWhi25vkjb0Rgjtaf6lx36Eh-2QQH1GhcDGlaZKXDwWtuwE_mCPyeA/exec";
 
 function getAllUrlParameters() {
 	const params = new URLSearchParams(window.location.search);
